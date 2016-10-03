0c280fbbc33e2b0f7a97999f7657b37aa422da9b
CurryBay <caleb.mah.2009@vjc.sg>
2016-10-03 20:29:05 +0800

    Changed XML file to add text views and button

--- app/src/main/java/com/example/beacon/beacon/MainActivity.java
+++ app/src/main/java/com/example/beacon/beacon/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.beacon.beacon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button scanButton = (Button) findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                //ADD WHAT HAPPENS WHEN BUTTON PRESS HERE
+            }
+        });
+
+
     }
 }
